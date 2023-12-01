e3295c30983936ae9620b36fc51ab3ae8431ed22
LevinhlamDoan <125123543+LevinhlamDoan@users.noreply.github.com>
2023-12-02 06:07:49 +1100

    Update app.py

--- project-pages/Web2IP/app.py
+++ project-pages/Web2IP/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index3.html")
 
 @app.route("/identify_ip", methods=["POST"])
 def identify_ip():
